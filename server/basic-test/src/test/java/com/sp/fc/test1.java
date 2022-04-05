28d0d09e895cef73da4c18ecdc1586c40f5d9d3f
devch96 <showme6601@gmail.com>
2022-04-05 17:27:41 +0900

    20220405

---
+++ server/basic-test/src/test/java/com/sp/fc/test1.java
@@ -0,0 +1,18 @@
+package com.sp.fc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class test1 {
+
+    @DisplayName("1. 테스트")
+    @Test
+    void test_1(){
+
+        Assertions.assertEquals("test","test");
+    }
+}
+
+
